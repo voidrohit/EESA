82785bf0b68fab82016f579a183b7acec64c2d87
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2021-06-16 12:52:38 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json())
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+  res.render('dashboard');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
